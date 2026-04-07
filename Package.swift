c2232363a9c76429ac7b011d505e086e475d855c
isaac9711 <isaac9711@jiran.com>
2026-04-07 19:01:41 +0900

    chore: convert to SPM project structure with Sparkle dependency

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "ClaudeNotify",
+    platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.0.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "ClaudeNotify",
+            dependencies: [
+                .product(name: "Sparkle", package: "Sparkle")
+            ],
+            path: "Sources/ClaudeNotify",
+            linkerSettings: [
+                .linkedFramework("SkyLight"),
+                .unsafeFlags([
+                    "-F", "/System/Library/PrivateFrameworks",
+                    "-Xlinker", "-rpath", "-Xlinker", "@executable_path/../Frameworks"
+                ])
+            ]
+        )
+    ]
+)
